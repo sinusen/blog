c9f9163777da6c7bc2dda1de4c513039a3648bf1
Sinu Sen <sen_sinu@yahoo.com.au>
2020-10-28 23:55:06 +1100

    Refactored the UserHeader to move the user selection logic to mapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -7,7 +7,7 @@ class UserHeader extends React.Component {
     this.props.fetchUser(this.props.userId);
   }
   render() {
-    const user = this.props.users.find((user) => user.id === this.props.userId);
+    const { user } = this.props;
 
     if (!user) {
       return null;
@@ -15,9 +15,9 @@ class UserHeader extends React.Component {
     return <div className="header">{user.name}</div>;
   }
 }
-const mapStateToProps = (state) => {
+const mapStateToProps = (state, ownProps) => {
   return {
-    users: state.users,
+    user: state.users.find((user) => user.id === ownProps.userId),
   };
 };
 export default connect(mapStateToProps, { fetchUser })(UserHeader);
